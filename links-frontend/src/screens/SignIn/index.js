0a49b3db92b855c0ed92d141664778fa62c0739e
isaiasluc <isaiiaslucena@gmail.com>
2020-07-01 17:33:57 -0300

    Redux added

--- links-frontend/src/screens/SignIn/index.js
+++ links-frontend/src/screens/SignIn/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {signIn} from './SignInActions';
 
-const SignIn = () => {
+const SignIn = (props) => {
+    const {account, signIn} = props;
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        signIn({email: 'isaiiaslucena@gmail.com', password: '123456'});
+    }
+
+    console.log('*** SignIn.account', account);
+
     return (
         <div className="container h-100 pt-5">
             <h1>Sign In</h1>
             <div className="d-flex flex-column h-100">
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className="form-group">
                         <label>Email</label>
                         <input type="text" className="form-control" />
                     </div>
                     <div className="form-group">
-                        <label>Senha</label>
+                        <label>Password</label>
                         <input type="password" className="form-control" />
                     </div>
                     <div>
@@ -28,4 +40,8 @@ const SignIn = () => {
     )
 }
 
-export default SignIn;
+const mapStateToProps = (state) => {
+    return { account: state.signIn.account };
+};
+
+export default connect(mapStateToProps, {signIn} )(SignIn);
