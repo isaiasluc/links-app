3cd824076d62f1e2ca6d066c78a6a2976363b1a0
isaiasluc <isaiiaslucena@gmail.com>
2020-07-01 18:12:04 -0300

    SignUp implemented

--- links-frontend/src/screens/SignUp/index.js
+++ links-frontend/src/screens/SignUp/index.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signUp } from './SignUpActions';
 
 const SignUp = () => {
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+        signUp(data);
+    };
+
     return (
         <div className="container h-100 pt-5">
             <h1>Sign Up</h1>
             <div className="d-flex flex-column h-100">
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className="form-group">
                         <label>Email</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" name="email" />
                     </div>
                     <div className="form-group">
                         <label>Senha</label>
-                        <input type="password" className="form-control" />
+                        <input type="password" className="form-control" name="password" />
                     </div>
                     <div className="form-group">
                         <label>Confirme a senha</label>
-                        <input type="password" className="form-control" />
+                        <input type="password" className="form-control" name="password_confirmation" />
                     </div>
                     <div>
                         <button className="btn btn-primary btn-round">Submit</button>
                     </div>
                 </form>
-                <div className="container text-center fixed-bottom pb-5">
+                <div className="container text-center pb-5">
                     <div className="text-muted">Já possui uma conta?</div>
                     <Link to='/sign-in'>Sign In</Link>
                 </div>
@@ -32,4 +42,9 @@ const SignUp = () => {
     )
 }
 
-export default SignUp;
+
+const mapStateToProps = (state) => {
+    return { account: state.signUp.account };
+};
+
+export default connect(mapStateToProps, {signUp} )(SignUp);
--- links-frontend/src/store.js
+++ links-frontend/src/store.js
@@ -2,10 +2,12 @@ import {createStore, applyMiddleware, combineReducers} from 'redux';
 import ReduxPromise from 'redux-promise';
 
 import signInReducer from './screens/SignIn/SignInReducer';
+import signUpReducer from './screens/SignUp/SignUpReducer';
 
 
 const reducers = combineReducers({
     signIn: signInReducer,
+    signUp: signInReducer,
 });
 
 const store = createStore(reducers, applyMiddleware(ReduxPromise));
