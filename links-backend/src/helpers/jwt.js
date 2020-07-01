35a5c7d03c7841e107ecf5330729a0a0aa92d66b
isaiasluc <isaiiaslucena@gmail.com>
2020-06-30 22:20:37 -0300

    Refresh token implemented

--- links-backend/src/helpers/jwt.js
+++ links-backend/src/helpers/jwt.js
@@ -24,4 +24,10 @@ const verifyRefreshJwt = (token) => {
     return jwt.verify(token, refreshTokenPrivateKey);
 }
 
-module.exports = { generateJwt, verifyJwt, generateRefreshJwt, verifyRefreshJwt };
+const getTokenFromHeaders = (headers) => {
+    const token = headers['authorization'];
+    return token ? token.slice(7, token.length) : null;
+};
+
+
+module.exports = { generateJwt, verifyJwt, generateRefreshJwt, verifyRefreshJwt, getTokenFromHeaders };
